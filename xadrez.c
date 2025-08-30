ced59349f0e07389e465ad381dbdaa2a4b0e5746
LucasBatalhaSecundino <202507321811@alunos.estacio.br>
2025-08-30 00:40:48 +0000

    AVENTUREIRO COMPLETO

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,7 @@ int main() {
     char peca, direcao;
     int i;
 
-    printf("Escolha uma peca (T - Torre, B - Bispo, R - Rainha): ");
+    printf("Escolha uma peca (T - Torre, B - Bispo, R - Rainha, C - Cavalo): ");
     scanf(" %c", &peca);
     if (peca == 'T') {
         printf("Você escolheu a torre.\n");
@@ -56,10 +56,76 @@ int main() {
             i++;
         }
         
-    } else{
+    } else if (peca == 'C')
+    {
+        printf("Você escolheu o cavalo.\n");
+        printf("O cavalo se move em L, pulando sobre as peças.\n");
+        printf("Movimento: duas casas para frente e uma para o lado.\n");
+        printf("Escolha uma direção(D = Cima direia, E = Cima esquerda, B = Baixo direita, V = Baixo esquerda): ");
+        scanf(" %c", &direcao);
+        if (direcao == 'D') {
+            int j = 0;
+            while (j < 1)
+            {
+                for (i = 0; i < 2; i++)
+                {
+                    printf("Cima\n");
+                }
+                printf("Direita\n");
+                j++;
+            }
+              } else if (direcao == 'E') {
+            int j = 0;
+            while (j < 1)
+            {
+                for (i = 0; i < 2; i++)
+                {
+                    printf("Cima\n");
+                }
+                printf("Esquerda\n");
+                j++;
+            }
+              } else if (direcao == 'B') {
+            int j = 0;
+            while (j < 1)
+            {
+                for (i = 0; i < 2; i++)
+                {
+                    printf("Baixo\n");
+                }
+                printf("Direita\n");
+                j++;
+            }
+              } else if (direcao == 'V') {
+            int j = 0;
+            while (j < 1)   
+            {
+                for (i = 0; i < 2; i++)
+                {
+                    printf("Baixo\n");
+                }
+                printf("Esquerda\n");
+                j++;
+            }
+            
+            
+           } else {
+            printf("Direção inválida.\n");
+           }
+        
+        
+        
+    }
+     
+    else{
         printf("Peça inválida.\n");
     }
 
     
+    
+    
+    
+
+
     return 0;
 }
